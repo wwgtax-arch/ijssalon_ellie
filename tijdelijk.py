079d90296641fe79ceca39d659ab27d8c61b0f38
Walter Tax <wwgtax@gmail.com>
2026-02-06 12:06:55 +0100

    bestand tijdelijk.py gecodeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,12 +1,21 @@
 
-prijs = 3 * 0.8
-reclame_tekst3 = f"vandaag in de aanbieding: 1 liter - slechts €{prijs:.2f}"
-reclame_tekst4 = reclame_tekst3.split()
-
-
-for el in reclame_tekst4:
+prijzen = {
+    "aardbei": 3,
+    "vanille": 4,
+    "chocolade": 5,
+    "aanbieding": (3*0.8),
+    " reclame_tekst": f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {(3*0.8)}",
+    " reclame_tekst2": f"vandaag in de aanbieding: vanille-ijs,  1 liter - slechts €{3 * 0.8:.2f}",
+    " reclame_tekst3": f"vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €{3 * 0.8:.2f}".upper(),
+    " reclame_tekst4": f"vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €{3 * 0.8:.2f}".lower().split(),
+}
+regels = []
+for el in prijzen[" reclame_tekst4"]:
     if len(el) >= 5:
         print(el.upper())
     else:
         print(el.lower())
+
+
+
 
